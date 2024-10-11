ce607b7b46b092e47142659b73cad05ebd874192
abui <adjiem31@gmail.com>
2024-10-11 23:09:06 +0700

    fix: err

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from api import views
 
 urlpatterns = [
     path('brands/<str:brand_id>/', views.get_recommendations, name='get_recommendations'),
-    path('generate_embedding/', views.generate_embedding, name='generate_embedding'),
+
 ]
